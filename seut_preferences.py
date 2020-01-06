9a5222e22725c661ef97fa4b10a251531d330277
enenra <enenra9@gmail.com>
2020-01-06 01:06:17 +0100

    Small update to add support for custom fbx importer path
    * doesn't do anything for now but will likely be needed for HKT export

--- seut_preferences.py
+++ seut_preferences.py
@@ -7,20 +7,25 @@ class SEUT_AddonPreferences(AddonPreferences):
     """Saves the preferences set by the user"""
     bl_idname = __package__
 
-    pref_havokPath: StringProperty(
-        name="Havok Standalone File Manager",
-        subtype='FILE_PATH',
-    )
     pref_mwmbPath: StringProperty(
         name="MWM Builder",
+        subtype='FILE_PATH',
+    )
+    pref_fbxImporterPath: StringProperty(
+        name="Custom FBX Importer",
+        subtype='FILE_PATH',
+    )
+    pref_havokPath: StringProperty(
+        name="Standalone File Manager",
         subtype='FILE_PATH',
     )
 
     def draw(self, context):
         layout = self.layout
+        layout.prop(self, "pref_mwmbPath", expand=True)
         box = layout.box()
-        box.label(text="External Executables")
+        box.label(text="Havok")
+        box.prop(self, "pref_fbxImporterPath", expand=True)
         box.prop(self, "pref_havokPath", expand=True)
-        box.prop(self, "pref_mwmbPath", expand=True)
 
         return
