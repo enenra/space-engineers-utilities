c0e5ca83f82c1c3af11a9f7b0d35f9908db65f4a
enenra <enenra9@gmail.com>
2020-01-15 17:28:37 +0100

    Initial prep for MWM support
    * Nothing functional yet.

---
+++ seut_ot_exportMWM.py
@@ -0,0 +1,41 @@
+import bpy
+import os
+
+from os.path                        import join
+from bpy.types                      import Operator
+from .seut_ot_recreateCollections   import SEUT_OT_RecreateCollections
+
+
+class SEUT_OT_ExportMWM(Operator):
+    """Compiles the MWM from the previously exported loose files"""
+    bl_idname = "object.export_mwm"
+    bl_label = "Compile MWM"
+    bl_options = {'REGISTER', 'UNDO'}
+
+
+    def execute(self, context):
+        """Compiles all loose files into a MWM"""
+
+        scene = context.scene
+        collections = SEUT_OT_RecreateCollections.get_Collections()
+        preferences = bpy.context.preferences.addons.get(__package__).preferences
+
+        if preferences.pref_looseFilesExportFolder == '1' and scene.prop_export_exportPath == "":
+            self.report({'ERROR'}, "SEUT: No export folder defined. (003)")
+            return {'CANCELLED'}
+
+        if preferences.pref_looseFilesExportFolder == '1' and scene.prop_export_exportPath.find("Models\\") == -1:
+            self.report({'ERROR'}, "SEUT: Export folder does not contain 'Models\\'. Cannot be transformed into relative path. (014)")
+            return {'CANCELLED'}
+
+        if scene.prop_subtypeId == "":
+            self.report({'ERROR'}, "SEUT: No SubtypeId set. (004)")
+            return {'CANCELLED'}
+            
+        # check whether files are present
+
+        # verify file paths
+
+        # call mwmb
+    
+        return {'FINISHED'}
