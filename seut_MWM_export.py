01c7f552cacfa93bb5ecc3758a336a7bf45657c8
Stollie <cheynestahlhut@gmail.com>
2020-01-19 02:14:01 +1000

    Removed extra log duplication on MWM export

--- seut_MWM_export.py
+++ seut_MWM_export.py
@@ -2,7 +2,10 @@ from .seut_ot_export        import ExportSettings
 
 def mwmbuilder(self, context, path, settings: ExportSettings, fbxfile: str, havokfile: str, paramsfile: str, mwmfile: str, materialspath):
 
-    scene = context.scene
-    cmdline = [settings.mwmbuilder, '/f', '/s:.\\', '/m:'+scene.prop_subtypeId+'.fbx', '/o:'+path+'', '/l:.\\MWMBuilder.log', '/x:'+materialspath+'']
-    # cmdline = [settings.mwmbuilder, '/s:Sources', '/m:'+basename+'.fbx', '/o:.\\' , '/x:'+settings.materialref , '/lod:'+settings.lodsmwmDir]
-    settings.callTool(cmdline, cwd=path, logfile=mwmfile+'.log')
+    try:
+        scene = context.scene
+        cmdline = [settings.mwmbuilder, '/f', '/s:.\\', '/m:'+scene.prop_subtypeId+'.fbx', '/o:'+path+'', '/x:'+materialspath+'']
+        # cmdline = [settings.mwmbuilder, '/s:Sources', '/m:'+basename+'.fbx', '/o:.\\' , '/x:'+settings.materialref , '/lod:'+settings.lodsmwmDir]
+        settings.callTool(cmdline, cwd=path, logfile=mwmfile+'.log')
+    finally:
+        self.report({'INFO'}, "SEUT: MWM file(s) have been created.")
