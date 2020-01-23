0c34bc9fda374fdb129471d75ac234460a818381
enenra <enenra9@gmail.com>
2020-01-23 17:05:15 +0100

    More functionality for structure conversion
    * iterate through scenes and do scene.name == subtypeId
    * iterate through scenes and add index to each scene name
    * iterate through collections (from old layers) and convert them to the SEUT collection structure

--- src/seut_ot_structureConversion.py
+++ src/seut_ot_structureConversion.py
@@ -22,18 +22,57 @@ class SEUT_OT_StructureConversion(Operator):
 
         # Set scene indexes and SubtypeIds
         for index in range(0, len(bpy.data.scenes)):
-            if bpy.data.scenes[index].seut.index == -1:
-                bpy.data.scenes[index].seut.index = index
-                bpy.data.scenes[index].seut.subtypeId = bpy.data.scenes[index].name
+            scn = bpy.data.scenes[index]
 
-                # For not active scenes, the update doesn't trigger apparently, so I have to add the index to the scene names manually.
+            if scn.seut.index == -1:
+                scn.seut.index = index
+                scn.seut.subtypeId = scn.name
+                # For inactive scenes, the update doesn't trigger apparently, so I have to add the index to the scene names manually.
                 if index > 0:
-                    bpy.data.scenes[index].name = bpy.data.scenes[index].name + ' (' + str(index) + ')'
+                    scn.name = scn.name + ' (' + str(scn.seut.index) + ')'
+            
+            # Create main SEUT collection for each scene
+            seut = bpy.data.collections.new('SEUT' + ' (' + str(scn.seut.index) + ')')
+            scn.collection.children.link(seut)
 
-        # port subparts in scenes to new layout
+            
+            # convert collections created from layers to corresponding SEUT collections
+            for collection in scn.collection.children:
 
-        # convert collections created from layers to corresponding SEUT collections
-
+                if collection.name == 'Collection 1' or collection.name[:13] == 'Collection 1.':
+                    collection.name = 'Main' + ' (' + str(scn.seut.index) + ')'
+                    
+                elif collection.name == 'Collection 2' or collection.name[:13] == 'Collection 2.':
+                    collection.name = 'Collision' + ' (' + str(scn.seut.index) + ')'
+                    
+                elif collection.name == 'Collection 3' or collection.name[:13] == 'Collection 3.':
+                    collection.name = 'MountPoints' + ' (' + str(scn.seut.index) + ')'
+                    
+                elif collection.name == 'Collection 4' or collection.name[:13] == 'Collection 4.':
+                    collection.name = 'Mirroring' + ' (' + str(scn.seut.index) + ')'
+                    
+                elif collection.name == 'Collection 6' or collection.name[:13] == 'Collection 6.':
+                    collection.name = 'LOD1' + ' (' + str(scn.seut.index) + ')'
+                    
+                elif collection.name == 'Collection 7' or collection.name[:13] == 'Collection 7.':
+                    collection.name = 'LOD2' + ' (' + str(scn.seut.index) + ')'
+                    
+                elif collection.name == 'Collection 8' or collection.name[:13] == 'Collection 8.':
+                    collection.name = 'LOD3' + ' (' + str(scn.seut.index) + ')'
+                    
+                elif collection.name == 'Collection 11' or collection.name[:14] == 'Collection 11.':
+                    collection.name = 'BS1' + ' (' + str(scn.seut.index) + ')'
+                    
+                elif collection.name == 'Collection 12' or collection.name[:14] == 'Collection 12.':
+                    collection.name = 'BS2' + ' (' + str(scn.seut.index) + ')'
+                    
+                elif collection.name == 'Collection 13' or collection.name[:14] == 'Collection 13.':
+                    collection.name = 'BS3' + ' (' + str(scn.seut.index) + ')'
+                
+                if not collection.name[:4] == 'SEUT':
+                    scn.collection.children.unlink(collection)
+                    bpy.data.collections['SEUT' + ' (' + str(scn.seut.index) + ')'].children.link(collection)
+                
         # convert LOD distances? and other variables
 
         # how to handle custom materials?
