de7d4c6a74b240273c0147e2ed6c1a4787ecb78c
enenra <enenra9@gmail.com>
2020-01-26 14:25:44 +0100

    Adjustments for empty spawning
    Unfortunately it doesn't look like it's possible to have the user select a scene from a list for an operator. As such the link is created empty and needs to be created via panel.

--- src/empties/seut_ot_addCustomSubpart.py
+++ src/empties/seut_ot_addCustomSubpart.py
@@ -9,11 +9,6 @@ class SEUT_OT_AddCustomSubpart(Operator):
     bl_label = "Add Custom Subpart"
     bl_options = {'REGISTER', 'UNDO'}
 
-    # Greys the button out if there is no active object.
-    @classmethod
-    def poll(cls, context):
-        return len(context.selected_objects) != 0
-
     prefix: StringProperty(
         name="Subpart Prefix",
         description="The prefix with which to combine the selected object's name to create the name for the empty",
@@ -21,40 +16,18 @@ class SEUT_OT_AddCustomSubpart(Operator):
     )
 
     def execute(self, context):
-        
-        targetObjects = bpy.context.view_layer.objects.selected
-        
-        if len(targetObjects) > 1:
-            self.report({'ERROR'}, "SEUT: Cannot create empties for more than one object at a time. (009)")
-            return {'CANCELLED'}
-        
-        # I need to figure out how I can get the first in the list but so far idk, this works
-        for obj in targetObjects:
-            targetObject = obj
             
         # Determine name strings.
         customPropName = "file"
 
         # Spawn empty
-        location = bpy.data.objects[targetObject.name].location
-        rotation = bpy.data.objects[targetObject.name].rotation_euler
-
-        xD = bpy.data.objects[targetObject.name].dimensions.x
-        yD = bpy.data.objects[targetObject.name].dimensions.y
-        zD = bpy.data.objects[targetObject.name].dimensions.z
-
-        bpy.ops.object.add(type='EMPTY', location=location, rotation=rotation)
-        bpy.ops.transform.resize(value=(xD - 1, yD - 1, zD - 1))
+        bpy.ops.object.add(type='EMPTY')
         empty = bpy.context.view_layer.objects.active
-        empty.scale = targetObject.scale
-
-        empty.parent = targetObject.parent
 
         empty.empty_display_type = "CUBE"
-        empty.name = self.prefix + targetObject.name
 
-        bpy.data.objects[empty.name][customPropName] = targetObject.name
+        bpy.data.objects[empty.name][customPropName] = ""
         
-        self.report({'INFO'}, "SEUT: Subpart '%s' created for file: '%s'" % (empty.name,targetObject.name))
+        self.report({'INFO'}, "SEUT: Subpart '%s' created for file: '%s'" % (empty.name,""))
         
         return {'FINISHED'}
