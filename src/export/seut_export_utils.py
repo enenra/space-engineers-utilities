890d2f20158ac4babf4cbbcbd339ed952659a6ca
Stollie <cheynestahlhut@gmail.com>
2020-01-27 11:53:18 +1000

    asd

--- src/export/seut_export_utils.py
+++ src/export/seut_export_utils.py
@@ -542,7 +542,7 @@ def export_to_fbxfile(settings: ExportSettings, scene, filepath, objects, ishavo
         'apply_unit_scale': True,
         'global_scale': 0.1, # STOLLIE: Is 1.0 in Blender Source
         'apply_scale_options': 'FBX_SCALE_NONE',
-        'axis_up': 'X',	 # STOLLIE: Normally a Z in Blender source.	Y aligns correctly in SE.
+        'axis_up': 'Y',	 # STOLLIE: Normally a Z in Blender source.	Y aligns correctly in SE.
         'axis_forward': 'Z', # STOLLIE: Normally a Y in Blender source. -Z is correct forward.
         'context_objects': objects, #STOLLIE: Is None in Blender Source.
         'object_types': {'MESH', 'EMPTY'}, # STOLLIE: Is None in Blender source.
