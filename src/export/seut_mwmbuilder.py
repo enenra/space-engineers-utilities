d2553f7574e5577d3a5efeb9b03b2bd4f2d51e38
Stollie <cheynestahlhut@gmail.com>
2020-01-28 21:24:22 +1000

    mwm export path corrections

--- src/export/seut_mwmbuilder.py
+++ src/export/seut_mwmbuilder.py
@@ -3,14 +3,13 @@ import glob
 
 from .seut_export_utils     import ExportSettings
 
-def mwmbuilder(self, context, path, settings: ExportSettings, mwmfile: str, materialspath):
+def mwmbuilder(self, context, path, mwmpath, settings: ExportSettings, mwmfile: str, materialspath):
     try:
         scene = context.scene
-        cmdline = [settings.mwmbuilder, '/f', '/s:'+path+'', '/m:'+scene.seut.subtypeId+'*.fbx', '/o:'+path+'', '/x:'+materialspath+'']
-        settings.callTool(cmdline, cwd=path, logfile=mwmfile+'.log')
-        # os.remove(path + scene.seut.subtypeId + ".hkt.mwm")
+        cmdline = [settings.mwmbuilder, '/f', '/s:'+path+'', '/m:'+scene.seut.subtypeId+'*.fbx', '/o:'+mwmpath+'', '/x:'+materialspath+'']
+        settings.callTool(cmdline, cwd=path, logfile=path + scene.seut.subtypeId + '.log')
     finally:
-        fileRemovalList = [fileName for fileName in glob.glob(path + "*.hkt.mwm")]
+        fileRemovalList = [fileName for fileName in glob.glob(mwmpath + "*.hkt.mwm")]
         try:
             for fileName in fileRemovalList:
                 os.remove(fileName)
