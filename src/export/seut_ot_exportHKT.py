23a833712fd8215ad9404dc687cf52464dd61477
enenra <enenra9@gmail.com>
2020-02-03 11:17:35 +0100

    Fix for HKT error when there are instances
    For some reason the apply_transformations operator runs on all objects instead of just the active one, so it tries to apply transformations to instances. That breaks a bunch, so I'm going to disable it until I can find a better solution.

--- src/export/seut_ot_exportHKT.py
+++ src/export/seut_ot_exportHKT.py
@@ -68,7 +68,7 @@ class SEUT_OT_ExportHKT(Operator):
 
         for obj in collections['hkt'].objects:
             context.view_layer.objects.active = obj
-            bpy.ops.object.transform_apply(location = True, scale = True, rotation = True)
+            # bpy.ops.object.transform_apply(location = True, scale = True, rotation = True) # This runs on all objects instead of just the active one for some reason. Breaks when there's instanced subparts.
             bpy.ops.rigidbody.object_add(type='ACTIVE')
 
         path = bpy.path.abspath(scene.seut.export_exportPath)
