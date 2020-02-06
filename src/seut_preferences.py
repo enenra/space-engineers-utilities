c2792993242db2a7d67068a90dae3869fb36574d
enenra <enenra9@gmail.com>
2020-02-06 09:38:58 +0100

    Descriptions for AddonPrefs external tools
    This fixes #97 .

--- src/seut_preferences.py
+++ src/seut_preferences.py
@@ -11,18 +11,22 @@ class SEUT_AddonPreferences(AddonPreferences):
 
     fbxImporterPath: StringProperty(
         name="Custom FBX Importer",
+        description="Despite its name, this tool is mainly used to export models to the FBX format",
         subtype='FILE_PATH',
     )
     havokPath: StringProperty(
         name="Havok Standalone Filter Manager",
+        description="This tool is required to create Space Engineers collision models",
         subtype='FILE_PATH',
     )
     mwmbPath: StringProperty(
         name="MWM Builder",
+        description="This tool converts the individual 'loose files' that the export yields into MWM files the game can read",
         subtype='FILE_PATH',
     )
     materialsPath: StringProperty(
         name="Materials Folder",
+        description="This folder contains material information in the form of XML libraries as well as BLEND MatLibs",
         subtype='FILE_PATH',
     )
 
