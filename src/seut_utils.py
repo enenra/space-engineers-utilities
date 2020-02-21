32e1ad255bdaa4d77a43d23317528307f4f2cf4c
enenra <enenra9@gmail.com>
2020-02-21 15:40:12 +0100

    Fix for edit mode issues with subpart instancing
    Fixes #131

--- src/seut_utils.py
+++ src/seut_utils.py
@@ -16,6 +16,9 @@ def linkSubpartScene(self, originScene, empty, targetCollection):
     subpartScene = empty.seut.linkedScene
     context.window.scene = subpartScene
 
+    if context.object is not None and context.object.mode is not 'OBJECT':
+        ops.object.mode_set(mode='OBJECT')
+
     subpartCollections = SEUT_OT_RecreateCollections.getCollections(subpartScene)
     # Checks whether collection exists, is excluded or is empty
     result = errorCollection(self, context, subpartCollections['main'], False)
