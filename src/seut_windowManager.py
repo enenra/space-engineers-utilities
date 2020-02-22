cfca36fa444d903716a0f67968e1f792ac0d6666
enenra <enenra9@gmail.com>
2020-02-22 15:47:13 +0100

    Improvements to MatLib handling
    * Enabled MatLib persist after reload
    * More optimization when enabling / disabling MatLibs
    
    This is related to #147 but doesn't fix it.

--- src/seut_windowManager.py
+++ src/seut_windowManager.py
@@ -33,12 +33,28 @@ def update_enabled(self, context):
         showError(context, "Report: Error", "SEUT Error: Path to Materials Folder (Addon Preferences) '" + materialsPath + "' not valid. (017)")
         return
 
+    if self.enabled:
+        with bpy.data.libraries.load(materialsPath + "\\" + self.name, link=True) as (data_from, data_to):
+            data_to.materials=data_from.materials
+
+    else:
+        with bpy.data.libraries.load(materialsPath + "\\" + self.name, link=True) as (data_from, data_to):
+                for mat in data_from.materials:
+                    if mat in bpy.data.materials and bpy.data.materials[mat].library is not None:
+                        bpy.data.materials.remove(bpy.data.materials[mat], do_unlink=True)
+                for img in data_from.images:
+                    if img in bpy.data.images and bpy.data.images[img].library is not None:
+                        bpy.data.images.remove(bpy.data.images[img], do_unlink=True)
+                for ngroup in data_from.node_groups:
+                    if ngroup in bpy.data.node_groups and bpy.data.node_groups[ngroup].library is not None:
+                        bpy.data.node_groups.remove(bpy.data.node_groups[ngroup], do_unlink=True)
+
+    """
     # Read MatLib materials.
     for lib in wm.seut.matlibs:
-        with bpy.data.libraries.load(materialsPath + "\\" + lib.name, link=True) as (data_from, data_to):
-
-            # If the lib is disabled, unlink its materials from the current file. (unless it's local)
-            if not lib.enabled:
+        # If the lib is disabled, unlink its materials from the current file. (unless it's local)
+        if not lib.enabled:
+            with bpy.data.libraries.load(materialsPath + "\\" + lib.name, link=True) as (data_from, data_to):
                 for mat in data_from.materials:
                     if mat in bpy.data.materials and bpy.data.materials[mat].library is not None:
                         bpy.data.materials.remove(bpy.data.materials[mat], do_unlink=True)
@@ -51,12 +67,11 @@ def update_enabled(self, context):
 
     # Read MatLib materials.
     for lib in wm.seut.matlibs:
-        with bpy.data.libraries.load(materialsPath + "\\" + lib.name, link=True) as (data_from, data_to):
-
-            # If the lib is enabled, link them to the current file.
-            if lib.enabled:
+        # If the lib is enabled, link them to the current file.
+        if lib.enabled:
+            with bpy.data.libraries.load(materialsPath + "\\" + lib.name, link=True) as (data_from, data_to):
                 data_to.materials = data_from.materials
-
+    """
 
 class SEUT_MatLibProps(PropertyGroup):
     """Holder for the various MatLib properties"""
