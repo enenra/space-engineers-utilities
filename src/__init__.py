6e68d72b5b8107023f1a70da51e9187621d8ff8e
enenra <enenra9@gmail.com>
2020-02-26 11:42:35 +0100

    version update

--- src/__init__.py
+++ src/__init__.py
@@ -15,7 +15,7 @@ bl_info = {
     "name": "Space Engineers Utilities",
     "description": "This addon offers various utilities to make creating assets for Space Engineers easier.",
     "author": "enenra, Stollie, Kamikaze",
-    "version": (0, 9, 83),
+    "version": (0, 9, 84),
     "blender": (2, 82, 0),
     "location": "View3D > Tools ",
     "warning": "",
