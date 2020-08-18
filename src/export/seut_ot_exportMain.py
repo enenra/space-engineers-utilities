7028fbc4b1331cd96a764ab3225976362a4aee73
enenra <enenra9@gmail.com>
2020-08-18 23:02:37 +0200

    Fix for new warning on .001 etc. empties.
    * Fixes #199

--- src/export/seut_ot_exportMain.py
+++ src/export/seut_ot_exportMain.py
@@ -81,7 +81,8 @@ class SEUT_OT_ExportMain(Operator):
 
         for obj in collections['main'].objects:
             if obj is not None and obj.type == 'EMPTY':
-                if re.search(".00*", obj.name[-4:]) != None or re.search(".0**", obj.name[-4:]) != None or re.search(".***", obj.name[-4:]) != None:
+                print("This: " + str(re.search(".00*", obj.name[-4:])))
+                if re.search("\.[0-9]{3}", obj.name[-4:]) != None:
                     self.report({'WARNING'}, "SEUT: Name of empty '%s' ends on '%s'. This might cause the empty to not work properly ingame." % (obj.name, obj.name[-4:]))
 
         # Export XML if boolean is set.
