604ff81305d3f6dc9b1f98fb39cae7d6c8454e67
enenra <enenra9@gmail.com>
2020-11-07 17:09:07 +0100

    Organizational adjustments

--- src/seut_errors.py
+++ src/seut_errors.py
@@ -5,75 +5,75 @@ import time
 
 
 errors = {
-    'E001': "SEUT: Import error. Imported object not found.",
-    'E002': "SEUT: Collection {variable_1} not found, excluded from view layer or empty. Action not possible.",
-    'E003': "SEUT: {variable_1} path '{variable_2}' doesn't exist.",
-    'E004': "SEUT: No SubtypeId set for scene '{variable_1}'.",
-    'E005': "SEUT: Linking to scene '{variable_1}' from '{variable_2}' would create a subpart instancing loop.",
-    'E006': "SEUT: LOD2 cannot be set if LOD1 is not, or LOD3 if LOD2 is not.",
-    'E007': "SEUT: '{variable_1}' texture filepath in local material '{variable_2}' does not contain 'Textures\\'. Cannot be transformed into relative path.",
-    'E008': "SEUT: BLEND file must be saved before export.",
-    'E009': "SEUT: Cannot create empties for more than one object at a time.",
-    'E010': "SEUT: Cannot run Simple Navigation if no SEUT collections are present.",
-    'E011': "SEUT: Invalid LOD distances. LOD2 cannot be set to be displayed before LOD1 or LOD3 before LOD2.",
-    'E012': "SEUT: Path to {variable_1} (Addon Preferences) '{variable_2}' not valid.",
-    'E013': "SEUT: Path to {variable_1} (Addon Preferences) not valid - wrong target file: Expected '{variable_2}' but is set to '{variable_3}'.",
-    'E014': "SEUT: Export path '{variable_1}' in scene '{variable_2}' does not contain 'Models\\'. Cannot be transformed into relative path.",
-    'E015': "SEUT: Invalid {variable_1} setup. Cannot have {variable_1}2 but no {variable_1}1, or {variable_1}3 but no {variable_1}2.",
-    'E016': "SEUT: Scene '{variable_1}' could not be exported.",
-    'E017': "SEUT: An error has occurred in the FBX exporter. Try exiting Edit-Mode before exporting.",
-    'E018': "SEUT: Cannot set SubtypeId to a SubtypeId that has already been used for another scene in the same BLEND file.",
-    'E019': "SEUT: No export folder defined for scene '{variable_1}'.",
-    'E020': "SEUT: Deletion of temporary files failed.",
-    'E021': "SEUT: Available MatLibs could not be refreshed.",
-    'E022': "SEUT: Too many objects in '{variable_1}'-collection. Collection contains {variable_2} but Space Engineers only supports a maximum of 16.",
-    'E023': "SEUT: Empty '{variable_1}' has incorrect rotation value: {variable_2}",
-    'E024': "SEUT: Collection 'Mountpoints ({variable_1})' not found. Disable and then re-enable Mountpoint Mode to recreate!",
-    'E025': "SEUT: Cannot create highlight empty for object outside of 'Main' collection.",
-    'E026': "SEUT: Filename incorrect: BLEND-filename must start with 'MatLib_' to create a valid MatLib.",
-    'E027': "SEUT: 'Mountpoints {variable_1}' not found. Disable and then re-enable Mountpoint Mode to recreate!",
-    'E028': "SEUT: Object is not an Armature.",
-    'E029': "SEUT: No Armature selected.",
-    'E030': "SEUT: Path is directory, not EXE.",
-    'E031': "SEUT: Cannot export collection '{variable_1}' if it has more than one top-level (unparented) object.",
-    'E032': "SEUT: Object '{variable_1}' does not have valid UV-Maps. This will crash Space Engineers.",
-    'E033': "SEUT: Invalid character(s) detected. This will prevent a MWM-file from being generated. Please ensure that no special (non ASCII) characters are used in SubtypeIds, material names and object names.",
-    'E034': "SEUT: Collision object '{variable_1}' has unapplied modifiers. Collision model cannot be created.",
-    'E035': "SEUT: There was an error during export caused by {variable_1}. Please refer to the logs in your export folder for details.",
+    'E001': "Import error. Imported object not found.",
+    'E002': "Collection {variable_1} not found, excluded from view layer or empty. Action not possible.",
+    'E003': "{variable_1} path '{variable_2}' doesn't exist.",
+    'E004': "No SubtypeId set for scene '{variable_1}'.",
+    'E005': "Linking to scene '{variable_1}' from '{variable_2}' would create a subpart instancing loop.",
+    'E006': "LOD2 cannot be set if LOD1 is not, or LOD3 if LOD2 is not.",
+    'E007': "'{variable_1}' texture filepath in local material '{variable_2}' does not contain 'Textures\\'. Cannot be transformed into relative path.",
+    'E008': "BLEND file must be saved before export.",
+    'E009': "Cannot create empties for more than one object at a time.",
+    'E010': "Cannot run Simple Navigation if no SEUT collections are present.",
+    'E011': "Invalid LOD distances. LOD2 cannot be set to be displayed before LOD1 or LOD3 before LOD2.",
+    'E012': "Path to {variable_1} (Addon Preferences) '{variable_2}' not valid.",
+    'E013': "Path to {variable_1} (Addon Preferences) not valid - wrong target file: Expected '{variable_2}' but is set to '{variable_3}'.",
+    'E014': "Export path '{variable_1}' in scene '{variable_2}' does not contain 'Models\\'. Cannot be transformed into relative path.",
+    'E015': "Invalid {variable_1} setup. Cannot have {variable_1}2 but no {variable_1}1, or {variable_1}3 but no {variable_1}2.",
+    'E016': "Scene '{variable_1}' could not be exported.",
+    'E017': "An error has occurred in the FBX exporter. Try exiting Edit-Mode before exporting.",
+    'E018': "Cannot set SubtypeId to a SubtypeId that has already been used for another scene in the same BLEND file.",
+    'E019': "No export folder defined for scene '{variable_1}'.",
+    'E020': "Deletion of temporary files failed.",
+    'E021': "Available MatLibs could not be refreshed.",
+    'E022': "Too many objects in '{variable_1}'-collection. Collection contains {variable_2} but Space Engineers only supports a maximum of 16.",
+    'E023': "Empty '{variable_1}' has incorrect rotation value: {variable_2}",
+    'E024': "Collection 'Mountpoints ({variable_1})' not found. Disable and then re-enable Mountpoint Mode to recreate!",
+    'E025': "Cannot create highlight empty for object outside of 'Main' collection.",
+    'E026': "Filename incorrect: BLEND-filename must start with 'MatLib_' to create a valid MatLib.",
+    'E027': "'Mountpoints {variable_1}' not found. Disable and then re-enable Mountpoint Mode to recreate!",
+    'E028': "Object is not an Armature.",
+    'E029': "No Armature selected.",
+    'E030': "Path is directory, not EXE.",
+    'E031': "Cannot export collection '{variable_1}' if it has more than one top-level (unparented) object.",
+    'E032': "Object '{variable_1}' does not have valid UV-Maps. This will crash Space Engineers.",
+    'E033': "Invalid character(s) detected. This will prevent a MWM-file from being generated. Please ensure that no special (non ASCII) characters are used in SubtypeIds, material names and object names.",
+    'E034': "Collision object '{variable_1}' has unapplied modifiers. Collision model cannot be created.",
+    'E035': "There was an error during export caused by {variable_1}. Please refer to the logs in your export folder for details.",
 }
 
 warnings = {
-    'W001': "SEUT: Collection not found. Action not possible.",
-    'W002': "SEUT: Collection '{variable_1}' excluded from view layer or empty. Action not possible.",
-    'W003': "SEUT: Could not remove unused material slots for object '{variable_1}'.",
-    'W004': "SEUT: '{variable_1}' texture of local material '{variable_2}' is not of a valid resolution ({variable_3}). May not display correctly ingame.",
-    'W005': "SEUT: Empty '{variable_1}' (numbering might differ) in collection '{variable_2}' has no parent object. This may prevent it from working properly ingame.",
-    'W006': "SEUT: Parent of empty '{variable_1}' (numbering might differ), '{variable_2}', in collection '{variable_3}' has a parent object. This may prevent the empty from working properly ingame.",
-    'W007': "SEUT: Highlight empty '{variable_1}' and its linked object '{variable_2}' have different parent objects. This may prevent the empty from working properly ingame.",
-    'W008': "SEUT: Scene '{variable_1}' is of type '{variable_2}' but does not contain any armatures.",
-    'W009': "SEUT: Scene '{variable_1}'  is of type '{variable_2}' but contains armatures.",
-    'W010': "SEUT: Library '{variable_1}' could not be relocated in '{variable_2}'.",
+    'W001': "Collection not found. Action not possible.",
+    'W002': "Collection '{variable_1}' excluded from view layer or empty. Action not possible.",
+    'W003': "Could not remove unused material slots for object '{variable_1}'.",
+    'W004': "'{variable_1}' texture of local material '{variable_2}' is not of a valid resolution ({variable_3}). May not display correctly ingame.",
+    'W005': "Empty '{variable_1}' (numbering might differ) in collection '{variable_2}' has no parent object. This may prevent it from working properly ingame.",
+    'W006': "Parent of empty '{variable_1}' (numbering might differ), '{variable_2}', in collection '{variable_3}' has a parent object. This may prevent the empty from working properly ingame.",
+    'W007': "Highlight empty '{variable_1}' and its linked object '{variable_2}' have different parent objects. This may prevent the empty from working properly ingame.",
+    'W008': "Scene '{variable_1}' is of type '{variable_2}' but does not contain any armatures.",
+    'W009': "Scene '{variable_1}'  is of type '{variable_2}' but contains armatures.",
+    'W010': "Library '{variable_1}' could not be relocated in '{variable_2}'.",
 }
 
 infos = {
-    'I001': "SEUT: Local material '{variable_1}' does not contain any valid textures. Skipping XML entry.",
-    'I002': "SEUT: Local material '{variable_1}' saved. Don't forget to include relevant DDS texture files in mod!",
-    'I003': "SEUT: Collection '{variable_1}' not found or empty. Skipping XML entry.",
-    'I004': "SEUT: '{variable_1}' has been created.",
-    'I005': "SEUT: IndexError at material '{variable_1}'.",
-    'I006': "SEUT: Export Options successfully copied to all scenes.",
-    'I007': "SEUT: FBX and XML files of scene '{variable_1}' have been compiled to MWM.",
-    'I008': "SEUT: {variable_1} of {variable_2} scenes successfully exported. Refer to Blender System Console for details.",
-    'I009': "SEUT: Collision files have been created.",
-    'I010': "SEUT: {variable_1} '{variable_2}' created.",
-    'I011': "SEUT: Highlight '{variable_1}' created for object '{variable_2}'.",
-    'I012': "SEUT: Structure conversion successfully completed.",
-    'I013': "SEUT: Attempt to Fix Positioning completed.",
-    'I014': "SEUT: Import successfully completed.",
-    'I015': "SEUT: Bounding Box set for dimensions X: {variable_1} Y: {variable_2} Z: {variable_3}",
-    'I016': "SEUT: Empty '{variable_1}' rotation {variable_2} registered as: {variable_3}",
-    'I017': "SEUT: Mountpoint Area {variable_1} saved. {variable_2} {variable_3}",
-    'I018': "SEUT: Icon successfully saved to '{variable_1}'.",
+    'I001': "Local material '{variable_1}' does not contain any valid textures. Skipping XML entry.",
+    'I002': "Local material '{variable_1}' saved. Don't forget to include relevant DDS texture files in mod!",
+    'I003': "Collection '{variable_1}' not found or empty. Skipping XML entry.",
+    'I004': "'{variable_1}' has been created.",
+    'I005': "IndexError at material '{variable_1}'.",
+    'I006': "Export Options successfully copied to all scenes.",
+    'I007': "FBX and XML files of scene '{variable_1}' have been compiled to MWM.",
+    'I008': "{variable_1} of {variable_2} scenes successfully exported. Refer to Blender System Console for details.",
+    'I009': "Collision files have been created.",
+    'I010': "{variable_1} '{variable_2}' created.",
+    'I011': "Highlight '{variable_1}' created for object '{variable_2}'.",
+    'I012': "Structure conversion successfully completed.",
+    'I013': "Attempt to Fix Positioning completed.",
+    'I014': "Import successfully completed.",
+    'I015': "Bounding Box set for dimensions X: {variable_1} Y: {variable_2} Z: {variable_3}",
+    'I016': "Empty '{variable_1}' rotation {variable_2} registered as: {variable_3}",
+    'I017': "Mountpoint Area {variable_1} saved. {variable_2} {variable_3}",
+    'I018': "Icon successfully saved to '{variable_1}'.",
 }
 
 
@@ -218,15 +218,13 @@ def seut_report(self, context, report_type: str, can_report: bool, code: str, va
         except KeyError:
             text = text.format(variable_1=variable_1)
 
-    link = "https://space-engineers-modding.github.io/modding-reference/tools/3d-modelling/seut/troubleshooting.html#" + code.lower()
-
     if report_type == 'ERROR':
         show_popup_report(context, "Report: Error", text)
-        print("Error: " + text + " (" + code + ")")
+        print("SEUT Error: " + text + " (" + code + ")")
     elif report_type == 'WARNING':
-        print("Warning: " + text + " (" + code + ")")
+        print("SEUT Warning: " + text + " (" + code + ")")
     elif report_type == 'INFO':
-        print("Info: " + text + " (" + code + ")")
+        print("SEUT Info: " + text + " (" + code + ")")
     
     add_to_issues(context, report_type, text, code, None)
 
@@ -250,7 +248,7 @@ def add_to_issues(context, issue_type: str, text: str, code: str, reference: str
     issue = issues.add()
     issue.timestamp = time.time()
     issue.issue_type = issue_type
-    issue.text = text[len("SEUT: "):]
+    issue.text = text[len(""):]
 
     if code is not None:
         issue.code = code
