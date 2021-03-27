f38efe2e8d6089e44892841fcdd0d68cad64ba68
enenra <enenra9@gmail.com>
2021-03-27 09:33:17 +0100

    Lights and Cameras can be unparented in main collection
    They aren't exported anyway, so this should be no issue.

--- src/export/seut_ot_export.py
+++ src/export/seut_ot_export.py
@@ -170,12 +170,12 @@ def export_main(self, context):
         if obj is not None and obj.type == 'ARMATURE':
             found_armatures = True
         
-        if obj.parent is None:
+        if obj.parent is None and obj.type != 'LIGHT' and obj.type != 'CAMERA':
             unparented_objects += 1
         
         # Check for missing UVMs (this might not be 100% reliable)
         if check_uvms(self, context, obj) != {'CONTINUE'}:
-            return {'CANCELLED'}  
+            return {'CANCELLED'}
     
     # Check for armatures being present in collection
     if not found_armatures and (scene.seut.sceneType == 'character' or scene.seut.sceneType == 'character_animation'):
