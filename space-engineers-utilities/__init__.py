8066aa7fd4d07ed95a79f7bdc348a849cc30abc7
enenra <enenra9@gmail.com>
2021-04-10 16:28:37 +0200

    0.9.95 Release version switch

--- space-engineers-utilities/__init__.py
+++ space-engineers-utilities/__init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "description": "This addon offers various utilities to make creating assets for Space Engineers easier.",
     "author": "enenra, Stollie",
     "version": (0, 9, 95),
-    "dev_version": 5,
+    "dev_version": 0,
     "dev_tag": "rc",
     "blender": (2, 92, 0),
     "location": "View3D > Tools",
