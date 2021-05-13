134742af7dd8960107ebcba0ffe6686b4543a071
enenra <enenra9@gmail.com>
2021-05-13 14:49:21 +0200

    Update seut_pt_toolbar.py

--- space-engineers-utilities/seut_pt_toolbar.py
+++ space-engineers-utilities/seut_pt_toolbar.py
@@ -362,7 +362,7 @@ class SEUT_PT_Panel_Export(Panel):
             row.prop(scene.seut, "export_smallGrid", icon='META_CUBE')
             if scene.seut.export_smallGrid and scene.seut.export_largeGrid:
                 row2 = box2.row()
-                row2.prop(scene.seut, "export_grid3To5Ratio")
+                row2.prop(scene.seut, "export_medium_grid", icon='CUBE')
         
         split = box.split(factor=0.85)
         col = split.column()
