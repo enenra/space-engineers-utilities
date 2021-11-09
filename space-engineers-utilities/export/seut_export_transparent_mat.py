817fc33bc94a2ec3659b79a897e0fe6c059e591e
enenra <enenra9@gmail.com>
2021-11-09 23:16:58 +0100

    TransparentMaterial transfer to new SBC update-system.

--- space-engineers-utilities/export/seut_export_transparent_mat.py
+++ space-engineers-utilities/export/seut_export_transparent_mat.py
@@ -12,105 +12,136 @@ def export_transparent_mat(self, context, subtype_id):
 
     scene = context.scene
     material = bpy.data.materials[subtype_id]
-    preferences = get_preferences()
     path_data = os.path.join(get_abs_path(scene.seut.mod_path), "Data")
     
     output = get_relevant_sbc(os.path.dirname(path_data), 'TransparentMaterials', subtype_id)
     if output is not None:
         file_to_update = output[0]
-        root = output[1]
-        element = output[2]
+        lines = output[1]
+        start = output[2]
+        end = output[3]
 
-    # Neither a TransparentMat-tree nor an entry for this particular one was found
-    if element is None:
+    # Neither a TransparentMat file nor an entry for this particular one was found
+    if file_to_update is None:
         definitions = ET.Element('Definitions')
         add_attrib(definitions, 'xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
         add_attrib(definitions, 'xmlns:xsd', 'http://www.w3.org/2001/XMLSchema')
-
         cube_blocks = add_subelement(definitions, 'TransparentMaterials')
         def_definition = add_subelement(cube_blocks, 'TransparentMaterial')
         update = False
     
     # TransparentMat-tree was found but no entry for this TransparentMat exists
-    elif root is not None:
-        definitions = root
+    elif file_to_update is not None and start is None and end is None:
+        tree = ET.parse(lines)
+        definitions = tree.getroot()
         for elem in definitions:
             if elem.tag == 'TransparentMaterials':
                 def_definition = add_subelement(elem, 'TransparentMaterial')
                 break
-        update = True
+        update = False
     
     # TransparentMat-tree & entry for this particular TransparentMat was found
     else:
-        definitions = root
-        def_definition = element
+        definitions = None
+        lines_entry = lines[start:end]
         update = True
     
-    add_subelement(def_definition, 'AlphaMistingEnable', material.seut.alpha_misting_enable, True)
-    add_subelement(def_definition, 'AlphaMistingStart', material.seut.alpha_misting_start, True)
-    add_subelement(def_definition, 'AlphaMistingEnd', material.seut.alpha_misting_end, True)
+    lines_entry = update_add_subelement(def_definition, 'AlphaMistingEnable', material.seut.alpha_misting_enable, update, lines_entry)
+    lines_entry = update_add_subelement(def_definition, 'AlphaMistingStart', material.seut.alpha_misting_start, update, lines_entry)
+    lines_entry = update_add_subelement(def_definition, 'AlphaMistingEnd', material.seut.alpha_misting_end, update, lines_entry)
     
-    add_subelement(def_definition, 'AlphaSaturation', material.seut.alpha_saturation, True)
+    lines_entry = update_add_subelement(def_definition, 'AlphaSaturation', material.seut.alpha_saturation, update, lines_entry)
     
-    add_subelement(def_definition, 'CanBeAffectedByOtherLights', material.seut.affected_by_other_lights, True)
+    lines_entry = update_add_subelement(def_definition, 'CanBeAffectedByOtherLights', material.seut.affected_by_other_lights, update, lines_entry)
     
-    add_subelement(def_definition, 'SoftParticleDistanceScale', material.seut.soft_particle_distance_scale, True)
+    lines_entry = update_add_subelement(def_definition, 'SoftParticleDistanceScale', material.seut.soft_particle_distance_scale, update, lines_entry)
     # TODO: CM texture reference
-    add_subelement(def_definition, 'Texture', 'placeholder', True)
+    lines_entry = update_add_subelement(def_definition, 'Texture', 'placeholder', update, lines_entry)
     
-    def_color = add_subelement(def_definition, 'Color')
-    add_subelement(def_color, 'X', material.seut.color[0], True)
-    add_subelement(def_color, 'Y', material.seut.color[1], True)
-    add_subelement(def_color, 'Z', material.seut.color[2], True)
-    add_subelement(def_color, 'W', material.seut.color[3] * (1 + material.seut.color_emission_multiplier), True)
+    if not update:
+        def_color = add_subelement(def_definition, 'Color')
+    else:
+        def_color = ET.Element(def_definition, 'Color')
+    add_subelement(def_color, 'X', material.seut.color[0])
+    add_subelement(def_color, 'Y', material.seut.color[1])
+    add_subelement(def_color, 'Z', material.seut.color[2])
+    add_subelement(def_color, 'W', material.seut.color[3] * (1 + material.seut.color_emission_multiplier))
+    if update:
+        lines_entry = convert_back_xml(def_color, 'Color', lines_entry)
     
-    def_color_add = add_subelement(def_definition, 'ColorAdd')
+    if not update:
+        def_color_add = add_subelement(def_definition, 'ColorAdd')
+    else:
+        def_color_add = ET.Element(def_definition, 'ColorAdd')
     add_subelement(def_color_add, 'X', material.seut.color_add[0])
     add_subelement(def_color_add, 'Y', material.seut.color_add[1])
     add_subelement(def_color_add, 'Z', material.seut.color_add[2])
-    add_subelement(def_color_add, 'W', material.seut.color_add[3] * (1 + material.seut.color_emission_multiplier), True)
-    
-    def_shadow_multiplier = add_subelement(def_definition, 'ShadowMultiplier')
-    add_subelement(def_shadow_multiplier, 'X', material.seut.shadow_multiplier[0], True)
-    add_subelement(def_shadow_multiplier, 'Y', material.seut.shadow_multiplier[1], True)
-    add_subelement(def_shadow_multiplier, 'Z', material.seut.shadow_multiplier[2], True)
-    add_subelement(def_shadow_multiplier, 'W', material.seut.shadow_multiplier[3], True)
-    
-    def_light_multiplier = add_subelement(def_definition, 'LightMultiplier')
-    add_subelement(def_light_multiplier, 'X', material.seut.light_multiplier[0], True)
-    add_subelement(def_light_multiplier, 'Y', material.seut.light_multiplier[1], True)
-    add_subelement(def_light_multiplier, 'Z', material.seut.light_multiplier[2], True)
-    add_subelement(def_light_multiplier, 'W', material.seut.light_multiplier[3], True)
-    
-    add_subelement(def_definition, 'Reflectivity', material.seut.reflectivity, True)
-    add_subelement(def_definition, 'Fresnel', material.seut.fresnel, True)
-    add_subelement(def_definition, 'ReflectionShadow', material.seut.reflection_shadow, True)
-
-    add_subelement(def_definition, 'GlossTextureAdd', material.seut.gloss_texture_add, True)
-    # TODO: NG texture reference
-    add_subelement(def_definition, 'Gloss', 'placeholder', True)
-
-    add_subelement(def_definition, 'SpecularColorFactor', material.seut.specular_color_factor, True)
-    add_subelement(def_definition, 'IsFlareOccluder', material.seut.is_flare_occluder, True)
-    
-    temp_string = ET.tostring(definitions, 'utf-8')
-    try:
-        temp_string.decode('ascii')
-    except UnicodeDecodeError:
-        seut_report(self, context, 'ERROR', True, 'E033')
-    xml_string = xml.dom.minidom.parseString(temp_string)
-    xml_formatted = xml_string.toprettyxml()
-    
-    xml_formatted = re.sub(r'\n\s*\n', '\n', xml_formatted)
-
+    add_subelement(def_color_add, 'W', material.seut.color_add[3] * (1 + material.seut.color_emission_multiplier))
     if update:
-        target_file = file_to_update
+        lines_entry = convert_back_xml(def_color, 'ColorAdd', lines_entry)
+    
+    if not update:
+        def_shadow_multiplier = add_subelement(def_definition, 'ShadowMultiplier')
     else:
+        def_shadow_multiplier = ET.Element(def_definition, 'ShadowMultiplier')
+    add_subelement(def_shadow_multiplier, 'X', material.seut.shadow_multiplier_x)
+    add_subelement(def_shadow_multiplier, 'Y', material.seut.shadow_multiplier_y)
+    add_subelement(def_shadow_multiplier, 'Z', material.seut.shadow_multiplier_z)
+    add_subelement(def_shadow_multiplier, 'W', material.seut.shadow_multiplier_w)
+    if update:
+        lines_entry = convert_back_xml(def_shadow_multiplier, 'ShadowMultiplier', lines_entry)
+    
+    if not update:
+        def_light_multiplier = add_subelement(def_definition, 'LightMultiplier')
+    else:
+        def_light_multiplier = ET.Element(def_definition, 'LightMultiplier')
+    add_subelement(def_light_multiplier, 'X', material.seut.light_multiplier_x)
+    add_subelement(def_light_multiplier, 'Y', material.seut.light_multiplier_y)
+    add_subelement(def_light_multiplier, 'Z', material.seut.light_multiplier_z)
+    add_subelement(def_light_multiplier, 'W', material.seut.light_multiplier_w)
+    if update:
+        lines_entry = convert_back_xml(def_light_multiplier, 'LightMultiplier', lines_entry)
+    
+    lines_entry = update_add_subelement(def_definition, 'Reflectivity', material.seut.reflectivity, update, lines_entry)
+    lines_entry = update_add_subelement(def_definition, 'Fresnel', material.seut.fresnel, update, lines_entry)
+    lines_entry = update_add_subelement(def_definition, 'ReflectionShadow', material.seut.reflection_shadow, update, lines_entry)
+
+    lines_entry = update_add_subelement(def_definition, 'GlossTextureAdd', material.seut.gloss_texture_add, update, lines_entry)
+    # TODO: NG texture reference
+    lines_entry = update_add_subelement(def_definition, 'Gloss', 'placeholder', update, lines_entry)
+
+    lines_entry = update_add_subelement(def_definition, 'SpecularColorFactor', material.seut.specular_color_factor, update, lines_entry)
+    lines_entry = update_add_subelement(def_definition, 'IsFlareOccluder', material.seut.is_flare_occluder, update, lines_entry)
+    
+    if not update:
+        temp_string = ET.tostring(definitions, 'utf-8')
+        try:
+            temp_string.decode('ascii')
+        except UnicodeDecodeError:
+            seut_report(self, context, 'ERROR', True, 'E033')
+        xml_string = xml.dom.minidom.parseString(temp_string)
+        xml_formatted = xml_string.toprettyxml()
+
+    else:
+        xml_formatted = lines.replace(lines[start:end], lines_entry)
+        xml_formatted = format_entry(xml_formatted)
+        target_file = file_to_update
+    
+    # This removes empty lines
+    # xml_formatted = re.sub(r'\n\s*\n', '\n', xml_formatted)
+
+    if file_to_update is None:
         target_file = os.path.join(path_data, "TransparentMaterials.sbc")
         if not os.path.exists(path_data):
             os.makedirs(path_data)
+    elif file_to_update is not None and start is None and end is None:
+        target_file = os.path.join(path_data, file_to_update)
+        if not os.path.exists(path_data):
+            os.makedirs(path_data)
+    else:
+        target_file = file_to_update
 
     exported_xml = open(target_file, "w")
     exported_xml.write(xml_formatted)
 
-    return
+    return {'FINISHED'}
