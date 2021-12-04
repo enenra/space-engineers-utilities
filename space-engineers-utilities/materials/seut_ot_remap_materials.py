52310d73b18e01926d219ddfd1df66a6145becaf
enenra <enenra9@gmail.com>
2021-12-04 19:28:14 +0100

    Rewrite remap materials

--- space-engineers-utilities/materials/seut_ot_remap_materials.py
+++ space-engineers-utilities/materials/seut_ot_remap_materials.py
@@ -1,9 +1,10 @@
 import bpy
+import os
 
 from bpy.types              import Operator
 
-from ..seut_errors          import seut_report
-from ..seut_utils           import prep_context
+from ..seut_errors          import get_abs_path, seut_report
+from ..seut_utils           import get_preferences, prep_context
 
 
 class SEUT_OT_RemapMaterials(Operator):
@@ -15,43 +16,66 @@ class SEUT_OT_RemapMaterials(Operator):
 
     def execute(self, context):
 
-        scene = context.scene
-        wm = context.window_manager
-
-        return
-        enabled = False
-        for lib in wm.seut.matlibs:
-            if lib.enabled:
-                enabled = True
-                break
-        
-        if enabled:
-            result = remap_materials(self, context)
-            return result
-        else:
-            seut_report(self, context, 'ERROR', True, 'E042')
-            return {'CANCELLED'}
+        return remap_materials(self, context)
 
     
 
 # The original version of this code was written by Kamikaze
 def remap_materials(self, context):
-    """Remap materials of objects in all scenes to linked library materials"""
+    """Remap materials of objects in all scenes to linked asset materials"""
 
+    preferences = get_preferences()
     current_scene = context.window.scene
     current_area = prep_context(context)
+
+    materials_path = os.path.join(get_abs_path(preferences.asset_path), 'Materials')
+    if not os.path.exists(materials_path):
+        seut_report(self, context, 'ERROR', True, 'E012', "Asset Directory", get_abs_path(preferences.asset_path))
+        return {'CANCELLED'}
+
+    blends = []
+    for file in os.listdir(materials_path):
+        if file is not None and file.endswith(".blend"):
+            blends.append(file)
+
+    if blends == []:
+        seut_report(self, context, 'ERROR', True, 'E021', materials_path)
+        return {'CANCELLED'}
+
+    available_mats = {}
+    for file in blends:
+        with bpy.data.libraries.load(os.path.join(materials_path, file), link=True) as (data_from, data_to):
+            data_to.materials = data_from.materials
+
+        for mat in data_to.materials:
+            if mat.asset_data is not None:
+                available_mats[mat.name] = file
+            if mat is not None and mat.users < 1:
+                bpy.data.materials.remove(mat, do_unlink=True)
+        
+        for img in data_from.images:
+            if img is not None and img in bpy.data.images:
+                img = bpy.data.images[img]
+                if img.users < 1:
+                    bpy.data.images.remove(img, do_unlink=True)
+                
+        for ng in data_from.node_groups:
+            if ng is not None and ng in bpy.data.node_groups:
+                ng = bpy.data.node_groups[ng]
+                if ng.users < 1:
+                    bpy.data.node_groups.remove(ng, do_unlink=True)
     
-    mtl_to_delete = []
+    if available_mats == {}:
+        seut_report(self, context, 'ERROR', True, 'E026', materials_path)
+        return {'FINISHED'}
 
     for scn in bpy.data.scenes:
         context.window.scene = scn
 
         for obj in context.view_layer.objects:
-
             if obj.type != 'MESH':
                 continue
 
-            # Select this object and remove any unused materials
             context.view_layer.objects.active = obj
 
             try:
@@ -60,29 +84,21 @@ def remap_materials(self, context):
                 seut_report(self, context, 'WARNING', True, 'W003', obj.name)
 
             for slot in obj.material_slots:
-                if slot.material != None and slot.material.library == None:
-                    # This material is not linked from a library
+                if slot.material is not None and slot.material.library is None:
                     old_material = slot.material
-
                     new_material = None
-                    # Try to find a linked material with the same name
-                    for mtl in bpy.data.materials:
-                        # If an object is imported that has a material that already exists in the scene, it is numbered.
-                        # Thus checking the substring of the name is necessary to catch all of them.
-                        if mtl.library != None and ( mtl.name == old_material.name or mtl.name == old_material.name[:-4] ) and old_material.seut.overrideMatLib == False:
-                            new_material = mtl
-                            break                            
 
-                    if new_material != None:
-                        # Use the linked material
-                        slot.material = new_material
-                        # Delete the old material
-                        if old_material not in mtl_to_delete:
-                            mtl_to_delete.append(old_material)
+                    if old_material.name in available_mats:
+                        new_material = old_material.name
+                    elif old_material.name[:-4] in available_mats:
+                        new_material = old_material.name[:-4]
 
-    for mtl in mtl_to_delete:
-        bpy.data.materials.remove(
-            mtl, do_unlink=True, do_id_user=True, do_ui_user=False)
+                    if new_material is not None:
+                        with bpy.data.libraries.load(os.path.join(materials_path, available_mats[new_material]), link=True) as (data_from, data_to):
+                            data_to.materials = [name for name in data_from.materials if name == new_material]
+                        for mat in bpy.data.materials:
+                            if mat is not None and mat.name == new_material and mat.library is not None:
+                                slot.material = mat
 
     context.area.type = current_area
     context.window.scene = current_scene
