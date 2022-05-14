44495b4433ba89afad0e24c6f742a1892ec40df5
enenra <enenra9@gmail.com>
2022-05-14 23:32:17 +0200

    Update seut_planet_io.py

--- space-engineers-utilities/planets/seut_planet_io.py
+++ space-engineers-utilities/planets/seut_planet_io.py
@@ -17,4 +17,6 @@ def bake_planet_map(scene: bpy.types.Scene, map_type: str = 'height'):
 def import_planet_sbc(path: os.path):
     """"""
 
+    # offer options for what parts should be imported? tickboxes.
+
     return
