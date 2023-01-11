99a544eeb26d8c35d35fdd0a31200416feac7543
enenra <enenra9@gmail.com>
2023-01-11 10:48:42 +0100

    Prevent actions from being duplicated endlessly when switching animation sets

--- space-engineers-utilities/seut_utils.py
+++ space-engineers-utilities/seut_utils.py
@@ -148,7 +148,9 @@ def link_subpart_scene(self, origin_scene, empty, target_collection):
                     # This ensures that the action of the original empty is associated with the linked one.
                     if linked_object.animation_data is not None:
                         if linked_object.animation_data.action is not None and linked_object.animation_data.action.name[:-4] in bpy.data.actions:
+                            duplicate_action = linked_object.animation_data.action
                             linked_object.animation_data.action = bpy.data.actions[linked_object.animation_data.action.name[:-4]]
+                            bpy.data.actions.remove(duplicate_action)
 
                     link_subpart_scene(self, origin_scene, linked_object, target_collection)
         
