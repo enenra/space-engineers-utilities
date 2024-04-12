f61fb235b04910414b0c18873185837a5fd741cf
enenra <enenra9@gmail.com>
2024-04-12 10:22:56 +0200

    Fix for QuickTools Bevel for Blender 4.1

--- space-engineers-utilities/quicktools/seut_quicktools_operators.py
+++ space-engineers-utilities/quicktools/seut_quicktools_operators.py
@@ -260,7 +260,10 @@ class SEUT_OT_QuickTools_MAIN_AddBevels(Operator):
         for obj in context.selected_objects:
             context.view_layer.objects.active = obj
 
-            bpy.ops.object.shade_smooth(use_auto_smooth=True)
+            if bpy.app.version < (4, 1, 0):
+                bpy.ops.object.shade_smooth(use_auto_smooth=True)
+            else:
+                bpy.ops.object.shade_smooth_by_angle()
 
             mods = {m.name for m in obj.modifiers}
             bpy.ops.object.modifier_add(type='BEVEL')
